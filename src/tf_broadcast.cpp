c006e1be7676ea04457ce66741acf4851dad0887
g-ch <947089399@qq.com>
2019-12-02 12:04:25 +0800

    add view field

--- src/tf_broadcast.cpp
+++ src/tf_broadcast.cpp
@@ -1,14 +1,18 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <sensor_msgs/Range.h>
+#include <math.h>
 
-tf::Transform transform;
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "tf_broadcaster");
     ros::NodeHandle node;
 
-    tf::TransformBroadcaster br;
+    ros::Publisher view_field_pub = node.advertise<sensor_msgs::Range>("/view_field",1);
 
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+	sensor_msgs::Range range;
 
     ros::Rate rate(20.0);
     while (node.ok()){
@@ -28,6 +32,14 @@ int main(int argc, char** argv){
         transform.setOrigin( tf::Vector3(0,0,0));
         transform.setRotation( tf::Quaternion(0, 0, sin(ros::Time::now().toSec()), 1) );
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "uav_link", "head_link"));
+
+        // range.header.frame_id = "head_link";
+        // range.radiation_type = 1;
+        // range.field_of_view = 3.14 / 3.0;
+        // range.min_range = 0.3;
+        // range.max_range = 10.0;
+        // range.range = 3.5;
+        // view_field_pub.publish(range);
 
         rate.sleep();
     }
